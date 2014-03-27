00d75322c8da94832fb21954bba2f106c8532442
athuan <thuannm56@gmail.com>
2014-03-27 10:49:12 +0700

    fix db migrate

---
+++ db/migrate/20140316025714_creat_entries.rb
@@ -0,0 +1,12 @@
+class CreatEntries < ActiveRecord::Migration
+  def change
+    create_table :entries do |t|
+      t.string :content
+      t.integer :user_id
+
+      t.timestamps
+    end
+    add_index :entries, [:user_id, :created_at]
+  end
+end
+
