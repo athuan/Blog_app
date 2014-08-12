0b0d3fca5b64a899ce261f3b1cae9ce7b49ccfad
athuan <thuannm56@gmail.com>
2014-08-12 16:02:07 +0700

    use mysql2

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20140324084256) do
     t.integer  "user_id"
   end
 
-  add_index "comments", ["entry_id", "created_at"], name: "index_comments_on_entry_id_and_created_at"
+  add_index "comments", ["entry_id", "created_at"], name: "index_comments_on_entry_id_and_created_at", using: :btree
 
   create_table "entries", force: true do |t|
     t.string   "content"
@@ -31,7 +31,16 @@ ActiveRecord::Schema.define(version: 20140324084256) do
     t.string   "title"
   end
 
-  add_index "entries", ["user_id", "created_at"], name: "index_entries_on_user_id_and_created_at"
+  add_index "entries", ["user_id", "created_at"], name: "index_entries_on_user_id_and_created_at", using: :btree
+
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at", using: :btree
 
   create_table "relationships", force: true do |t|
     t.integer  "follower_id"
@@ -40,9 +49,9 @@ ActiveRecord::Schema.define(version: 20140324084256) do
     t.datetime "updated_at"
   end
 
-  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
-  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
-  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id", using: :btree
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true, using: :btree
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -54,7 +63,7 @@ ActiveRecord::Schema.define(version: 20140324084256) do
     t.boolean  "admin",           default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
